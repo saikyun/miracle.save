c0cb2475e98efb3061ea47ef364ed43de877e13b
Jona <saikyun@gmail.com>
2019-06-14 11:53:52 +0200

    updated readme and project.clj

--- project.clj
+++ project.clj
@@ -1,2 +1,7 @@
 (defproject miracle-save "0.0.1"
+  :description "Tiny clojure lib for saving/loading local bindings, useful for debugging."
+  :license {:name "MIT License"
+            :url "https://choosealicense.com/licenses/mit/"
+            :distribution :repo}
+  :url "https://github.com/Saikyun/miracle.save"
   :dependencies [[org.clojure/clojure "1.9.0"]])
